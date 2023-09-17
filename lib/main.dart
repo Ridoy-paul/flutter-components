389bea49a4abddf7de51a742fea9e308954bab29
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-09-17 16:40:14 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
@@ -21,8 +22,29 @@ class HomePage extends StatelessWidget {
         title: Text("My AppBar"),
         centerTitle: true,
         backgroundColor: Colors.amberAccent,
+
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+
+          },
+        ),
+        actions: [
+          IconButton(
+              onPressed: (){},
+              icon: Icon(Icons.shopping_cart)
+          ),
+          IconButton(
+              onPressed: (){},
+              icon: Icon(Icons.wifi_calling_sharp)
+          ),
+          IconButton(
+              onPressed: (){},
+              icon: Icon(Icons.account_circle)
+          ),
+
+        ],
       ),
     );
   }
-
 }
