59b8ad487425452509dd8c77a00b661e10788113
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-09-19 20:13:09 +0600

    BD flag degined into container

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
@@ -27,13 +28,39 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
       ),
       body: SafeArea(
+        child: Column(
+          children: [
+            Center(
+              child: Container(
+                height: 150,
+                width: MediaQuery.of(context).size.width * 0.9,
+                margin: EdgeInsets.all(10.0),
+                color: Colors.green,
+                alignment: Alignment.center,
+                
+                child: Container(
+                  height: 80.0,
+                  width: 80.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.red,
+                  ),
+
+                ),
+              ),
+            )
+          ],
+        ),
+        /*
         child: Container(
           height: 150.0,
           width: 300.0,
+          margin: EdgeInsets.only(left: 20.0, top: 20.0),
           alignment: Alignment.center,
           child: Container(
             height: 80.0,
             width: 80.0,
+
             decoration: BoxDecoration(
               shape: BoxShape.circle,
               color: Colors.red,
@@ -43,6 +70,7 @@ class HomePage extends StatelessWidget {
             color: Colors.green,
           ),
         ),
+        */
       ),
     );
   }
