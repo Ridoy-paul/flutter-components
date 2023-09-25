91023048ee577463545f63d5bf06c6361b283442
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-09-26 02:19:53 +0600

    aspect ratio updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(CounterApp());
+  return runApp(MyAcc());
 }
 
-class CounterApp extends StatefulWidget {
-  const CounterApp({super.key});
+class MyAcc extends StatelessWidget {
+  const MyAcc({super.key});
 
-  @override
-  State<CounterApp> createState() => _CounterAppState();
-}
-
-class _CounterAppState extends State<CounterApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+        home: HomeScreen()
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int count = 0;
-
-  void _countUp() {
-    setState(() {
-      count++;
-      if(count >= 5) {
-        print("up");
-      }
-    });
-  }
-
-  void _countDown() {
-    setState(() {
-      if(count > 0) {
-        count--;
-      }
-      if(count >= 5) {
-        print("up");
-      }
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Counter App $count"),
-        backgroundColor: Colors.red,
+        title: Text("Aspect Ratio"),
       ),
       body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: EdgeInsets.all(3),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Count", style: TextStyle(fontSize: 30),),
-                Text("$count", style: TextStyle(fontSize: 50),),
-                SizedBox(height: 20,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(onPressed: (){
-                       _countDown();
-                    }, child: Icon(Icons.remove)),
-                    SizedBox(width: 20,),
-                    ElevatedButton(onPressed: (){
-                      _countUp();
-                    }, child: Icon(Icons.add)),
-                  ],
-                )
-              ],
+        child: Container(
+          height: 300,
+          width: double.infinity,
+          color: Colors.deepOrange,
+          alignment: Alignment.center,
+          child: AspectRatio(
+            aspectRatio: 6/2,
+            child: Container(
+              alignment: Alignment.center,
+              height: 100,
+              width: 100,
+              color: Colors.lightBlue,
+              child: Text("hello"),
             ),
           ),
         ),
@@ -88,5 +46,4 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
-
 
