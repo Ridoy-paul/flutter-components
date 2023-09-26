b76172f011775a5a3dc0509dec0a9b955d094ac9
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-09-26 23:38:31 +0600

    media query in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  return runApp(MyAcc());
+void main(){
+  return runApp(MyApp());
 }
 
-class MyAcc extends StatelessWidget {
-  const MyAcc({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-        home: HomeScreen()
+      home: HomeScreen(),
     );
   }
 }
@@ -21,26 +23,64 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
+    Orientation displayOrientation = MediaQuery.orientationOf(context);
+    List<DisplayFeature> displayFeatures = MediaQuery.displayFeaturesOf(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Aspect Ratio"),
+        title: Text('Media Query in Flutter'),
       ),
       body: SafeArea(
-        child: Container(
-          height: 300,
-          width: double.infinity,
-          color: Colors.deepOrange,
-          alignment: Alignment.center,
-          child: AspectRatio(
-            aspectRatio: 6/2,
-            child: Container(
-              alignment: Alignment.center,
-              height: 100,
-              width: 100,
-              color: Colors.lightBlue,
-              child: Text("hello"),
-            ),
-          ),
+        child: Center(
+          child: Column(
+
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Screen Size: ${size}"),
+              Text("Width: ${size.width}"),
+              Text("Height: ${size.height}"),
+              Text("Aspect Ratio: ${size.aspectRatio}"),
+              Text("Flipped Info: ${size.flipped}"),
+              Text("Orientation: ${displayOrientation}"),
+              Text("Display Features: ${displayFeatures}"),
+              SizedBox(height: 20,),
+              Container(
+                child: SizedBox(
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: SizedBox(
+                          width: MediaQuery.sizeOf(context).width/2.2,
+                          height: MediaQuery.sizeOf(context).width/2,
+                          child: Card(
+                            color: Colors.deepOrange,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: SizedBox(
+                          width: MediaQuery.sizeOf(context).width/2.2,
+                          height: MediaQuery.sizeOf(context).width/2,
+                          child: Card(
+                            color: Colors.deepOrange,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                      ),
+
+                    ],
+                  ),
+                ),
+              )
+            ],
+          )
         ),
       ),
     );
