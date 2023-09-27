d368a6a4376e1e38cbad0d24aa9da95b1b4430d1
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-09-28 02:27:02 +0600

    bootstrap flutter added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'style.dart';
+import 'package:flutter_bootstrap/flutter_bootstrap.dart';
 
-void main(){
-  return runApp(MyApp());
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,24 +12,79 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: HomePage(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomePage extends StatelessWidget {
+   HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        title: Text('Responsive Grid'),
+        title: Text("Flutter Bootstrap"),
+        backgroundColor: Colors.deepOrange,
       ),
       body: SafeArea(
-        child: Text("hello"),
+        child: SingleChildScrollView(
+          child: BootstrapContainer(
+            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 0),
+            fluid: true,
+            children: <Widget>[
+              BootstrapRow(
+                  children: [
+                    BootstrapCol(
+                      sizes: 'col-6 col-sm-6 col-md-4 col-xl-4',
+                      child: ContentWidget(
+                        color: Colors.deepOrange,
+                        text: 'col-6 col-sm-6',
+                      ),
+                    ),
+                    BootstrapCol(
+                      sizes: 'col-6 col-sm-6 col-md-4 col-xl-4',
+                      child: ContentWidget(
+                        color: Colors.deepOrange,
+                        text: 'col-6 col-sm-6',
+                      ),
+                    ),
+                    BootstrapCol(
+                      sizes: 'col-12 col-sm-6 col-md-4 col-xl-4',
+                      child: ContentWidget(
+                        color: Colors.deepOrange,
+                        text: 'col-12 col-sm-6',
+                      ),
+                    ),
+
+
+                  ],
+              ),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class ContentWidget extends StatelessWidget {
+  const ContentWidget({
+    Key? key,
+    required this.text,
+    required this.color,
+  }) : super(key: key);
+
+  final String text;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(0, 0, 0, 5),
+      height: 50,
+      color: color,
+      child: Center(child: Text(text, style: TextStyle(color: Colors.white),)),
     );
   }
 }
